e96323b9ef3ea36c2f435275100607a399f1e8cb
tonytomo <fathonisatrioutomo@gmail.com>
2025-09-04 16:19:37 +0700

    feat: Deploy the app to github page

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({ fallback: '404.html' }),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
 
 export default config;
